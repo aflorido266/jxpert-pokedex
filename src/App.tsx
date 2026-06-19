e6bb4d34391ac6f331a7f925258863312d60ed6f
aflorido266 <aflorido266@gmail.com>
2026-06-19 15:57:02 +0200

    add SearchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
 import { usePokemonList } from "./ui/hooks/usePokemonList";
 import { REGIONS } from "./infrastructure/api/regions";
 import { PokemonGrid } from "./ui/components/organisms/PokemonGrid";
-import { Footer } from "./ui/components/organisms/footer
+import { Footer } from "./ui/components/organisms/Footer";
 import { Header } from "./ui/components/organisms/Header";
+import { SearchBar } from "./ui/components/organisms/SearchBar";
 
 const regs = REGIONS.map((r) => r.name);
 
 export const App = () => {
   const {
-    ldr, fltr,
+    ldr,
+    fltr,
     finalResult,
-    busqueda, setBusqueda,
-    reg, setreg,
-    showregs, setShowregs,
-    showSort, setShowSort,
-    sorting, setSort,
+    busqueda,
+    setBusqueda,
+    reg,
+    setreg,
+    showregs,
+    setShowregs,
+    showSort,
+    setShowSort,
+    sorting,
+    setSort,
   } = usePokemonList();
 
   return (
     <div className="layout">
       <Header />
       <main className="container">
-        <section className="search">
-          {/* Buscador — igual que antes */}
-          ...
-        </section>
+        <SearchBar
+          busqueda={busqueda}
+          setBusqueda={setBusqueda}
+          reg={reg}
+          setreg={setreg}
+          showregs={showregs}
+          setShowregs={setShowregs}
+        />
         <section>
           {(ldr || fltr) && (
             <div className="grid" aria-hidden="true">
               {Array.from({ length: 6 }, (_, index) => (
-                <article key={`placeholder-card-${index}`} className="card card-placeholder">
+                <article
+                  key={`placeholder-card-${index}`}
+                  className="card card-placeholder"
+                >
                   <svg viewBox="0 0 24 24">
                     <path d="M12,2C17.52,2 22,6.48 22,12..." />
                   </svg>
---
+++ src/ui/components/organisms/SearchBar.tsx
@@ -0,0 +1,91 @@
+type SearchBarProps = {
+  busqueda: string;
+  setBusqueda: (value: string) => void;
+
+  reg: string;
+  setreg: (value: string) => void;
+
+  showregs: boolean;
+  setShowregs: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+
+export function SearchBar({
+  busqueda,
+  setBusqueda,
+  reg,
+  setreg,
+  showregs,
+  setShowregs,
+}: SearchBarProps) {
+  return (
+    <section className="search">
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        className="search__icon"
+      >
+        <path
+          d="M3 10C3 10.9193 3.18106 11.8295 3.53284 12.6788C3.88463 13.5281 4.40024 14.2997 5.05025 14.9497C5.70026 15.5998 6.47194 16.1154 7.32122 16.4672C8.1705 16.8189 9.08075 17 10 17C10.9193 17 11.8295 16.8189 12.6788 16.4672C13.5281 16.1154 14.2997 15.5998 14.9497 14.9497C15.5998 14.2997 16.1154 13.5281 16.4672 12.6788C16.8189 11.8295 17 10.9193 17 10C17 9.08075 16.8189 8.1705 16.4672 7.32122C16.1154 6.47194 15.5998 5.70026 14.9497 5.05025C14.2997 4.40024 13.5281 3.88463 12.6788 3.53284C11.8295 3.18106 10.9193 3 10 3C9.08075 3 8.1705 3.18106 7.32122 3.53284C6.47194 3.88463 5.70024 4.40024 5.05025 5.05025C4.40024 5.70026 3.88463 6.47194 3.53284 7.32122C3.18106 8.1705 3 9.08075 3 10Z"
+          stroke="var(--color-neutral-400)"
+          strokeWidth="2.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M21 21L15 15"
+          stroke="var(--color-neutral-400)"
+          strokeWidth="2.75"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+
+      <input
+        type="text"
+        placeholder="Search a Pokémon..."
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+      />
+<div className="dropdown">
+  <button
+    role="combobox"
+    aria-haspopup="listbox"
+    aria-controls="reg-list"
+    aria-label="Select reg"
+    aria-expanded={showregs}
+    className={`dropdown__button ${showregs ? "active" : ""}`}
+    onClick={() => setShowregs((prev) => !prev)}
+  >
+    {reg}
+
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.33337 5.99999L8.00004 3.33333L10.6667 5.99999"
+        stroke="var(--color-neutral-600)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.6667 10L8.00004 12.6667L5.33337 10"
+        stroke="var(--color-neutral-600)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  </button>
+</div>
+
+    </section>
+  );
+}
